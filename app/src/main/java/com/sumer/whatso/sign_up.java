4ba14648627065f98cbf52fa9d833f801cc7f4bf
Sumer Kumar <sumer47kumar@gmail.com>
2021-02-27 18:55:03 +0530

    user sign up activity layout created

---
+++ app/src/main/java/com/sumer/whatso/sign_up.java
@@ -0,0 +1,16 @@
+package com.sumer.whatso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
